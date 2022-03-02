e68f46548e8c6da59534787c37ec15895a41dcfe
Jonathan Miller <jon@jmill.dev>
2022-03-02 15:55:51 -0800

    Add role mention

--- abraham.py
+++ abraham.py
@@ -10,15 +10,12 @@ from marsbots_core.resources.language_models import OpenAIGPT3LanguageModel
 from marsbots_core.resources.discord_utils import (
     get_discord_messages,
     is_mentioned,
+    remove_role_mentions,
     replace_bot_mention,
     replace_mentions_with_usernames,
+    role_is_mentioned,
 )
-from . import (
-    settings,
-    prompts,
-    channels,
-    utils
-)
+from . import settings, prompts, channels, utils
 
 
 class AbrahamCog(commands.Cog):
@@ -28,7 +25,7 @@ class AbrahamCog(commands.Cog):
             engine=settings.GPT3_ENGINE,
             temperature=settings.GPT3_TEMPERATURE,
             frequency_penalty=settings.GPT3_FREQUENCY_PENALTY,
-            presence_penalty=settings.GPT3_PRESENCE_PENALTY
+            presence_penalty=settings.GPT3_PRESENCE_PENALTY,
         )
 
     @commands.Cog.listener("on_message")
@@ -36,51 +33,55 @@ class AbrahamCog(commands.Cog):
         dm = isinstance(message.channel, discord.channel.DMChannel)
         dm_allowed = dm and message.author.id in channels.ALLOWED_DM_USERS
         if (
-            (is_mentioned(message, self.bot.user) or dm_allowed)
+            (
+                is_mentioned(message, self.bot.user)
+                or role_is_mentioned(message)
+                or dm_allowed
+            )
             and message.author.id != self.bot.user.id
             and (dm_allowed or message.channel.id in channels.ALLOWED_CHANNELS)
         ):
             ctx = await self.bot.get_context(message)
 
             question = message.content
-            url = os.environ['SERVER_URL']
-            password = os.environ['SERVER_PASSWORD']
-            folder = os.path.join(os.getcwd(), 'results')
-            headers = {'Content-Type': 'application/json'}
-                        
-            data = {"question": question, "password": password}
-            result = requests.post(url+'/run', json=data, headers=headers).json()
+            url = os.environ["SERVER_URL"]
+            password = os.environ["SERVER_PASSWORD"]
+            folder = os.path.join(os.getcwd(), "results")
+            headers = {"Content-Type": "application/json"}
 
-            if 'token' not in result:
+            data = {"question": question, "password": password}
+            result = requests.post(url + "/run", json=data, headers=headers).json()
+
+            if "token" not in result:
                 async with ctx.channel.typing():
-                    await message.reply('Something went wrong :(')
+                    await message.reply("Something went wrong :(")
                 return
 
-            token = result['token']
+            token = result["token"]
             async with ctx.channel.typing():
                 await message.reply("I will get back to you")
 
             finished = False
             while not finished:
                 data = {"token": token, "password": password}
-                result = requests.post(url+'/fetch', json=data, headers=headers)
+                result = requests.post(url + "/fetch", json=data, headers=headers)
                 result = result.json()
-                status = result['status']['status']
-                if status == 'complete':
-                    output = result['output']
-                    response = output['response']
-                    filename = utils.download_file(url+'/'+output['video'], folder)
-                    filepath = f'{folder}/{filename}'
+                status = result["status"]["status"]
+                if status == "complete":
+                    output = result["output"]
+                    response = output["response"]
+                    filename = utils.download_file(url + "/" + output["video"], folder)
+                    filepath = f"{folder}/{filename}"
                     finished = True
                     async with ctx.channel.typing():
                         local_file = discord.File(filepath, filename=filepath)
                         await message.reply(response, file=local_file)
-                elif status == 'failed':
+                elif status == "failed":
                     finished = True
                     async with ctx.channel.typing():
-                        await message.reply('Something went wrong :(')
+                        await message.reply("Something went wrong :(")
                 else:
-                    time.sleep(1)  
+                    time.sleep(1)
 
     def format_prompt(self, messages):
         last_message_content = replace_bot_mention(messages[-1].content).strip()
@@ -94,13 +95,18 @@ class AbrahamCog(commands.Cog):
             + "\n"
             + messages_content
             + "\n"
-            + "**["+self.bot.user.name+"]**:"
+            + "**["
+            + self.bot.user.name
+            + "]**:"
         )
         return prompt
 
     def format_prompt_messages(self, messages):
         return "\n".join(
-            ["**[%s]**: %s"%(message['sender'], message['message']) for message in messages]
+            [
+                "**[%s]**: %s" % (message["sender"], message["message"])
+                for message in messages
+            ]
         )
 
     def format_messages(self, messages_content):
@@ -128,6 +134,7 @@ class AbrahamCog(commands.Cog):
         message_content = replace_mentions_with_usernames(
             message_content, message.mentions
         )
+        message_content = remove_role_mentions(message_content)
         message_content = message_content.strip()
         return message_content
 
